f8b8151d3d45dfa14b15dd2b8c20afac218d5f68
Thomas Quinlan <tquinlan1992@gmail.com>
2017-11-02 01:19:35 -0400

    set up docker-compose with mongo

--- src/connectMongoose.ts
+++ src/connectMongoose.ts
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose.Promise = require('bluebird');
-const mongo_url = 'mongodb://' + process.env.MONGODB_PORT_27017_TCP_ADDR + ':' + process.env.MONGODB_PORT_27017_TCP_PORT;
-console.log("process.env", process.env);
-console.log("mongo_url environment", mongo_url);
-mongoose.connection.openUri(mongo_url).then(() => {
-    console.log("Successfully connected to MONGO_URL: " + mongo_url);
+const mongoUrl = process.env.MONGO_URL;
+mongoose.connection.openUri(mongoUrl).then(() => {
+    console.log("Successfully connected to MONGO_URL: " + mongoUrl);
 }, (err: any) => {
-    console.error("Error connecting to MONGO_URL: " + mongo_url);
+    console.error("Error connecting to MONGO_URL: " + mongoUrl);
 });
